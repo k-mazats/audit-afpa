950ae1a03188010318619321ae5dd288acbc2aa4
k-mazats <k.mazats@gmail.com>
2020-04-01 16:34:44 +0200

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Audit AFPA</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="index.php?page=medias">Médias</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
